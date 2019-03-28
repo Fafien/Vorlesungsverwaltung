e4ebac92f146f355d5a19b256ff22fe1bef783fd
AnsgarLichter <ansgar.lichter@dh-karlsruhe.de>
2019-03-28 21:00:20 +0100

    Added LectureResource for Webservice, GET-Methods are not implemented yet

---
+++ Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/lectures/rest/LectureResource.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2019 Dennis Schulmeister-Zimolong
+ *
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ *
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package vorlesungsverwaltung.lectures.rest;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import vorlesungsverwaltung.lectures.ejb.LectureBean;
+
+/**
+ *
+ * @author Licht
+ */
+@Path("Lectures")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class LectureResource {
+
+    @EJB
+    private LectureBean lectureBean;
+
+    //TODO: Methoden des Webservices hinzufügen:
+    /*Anforderungen an den Webservice sind:
+    * Ändernde Zugriffe sind nicht zwingend notwendig, allerdings
+    * verschiedene Suchfunktionen:
+    *
+    * GET Methode für Vorlesungen (alle, Filter über Name oder ID, Filter per Kurs)
+    * GET Methode für Kurse (alle, Filter über Name)
+    *
+     */
+}
