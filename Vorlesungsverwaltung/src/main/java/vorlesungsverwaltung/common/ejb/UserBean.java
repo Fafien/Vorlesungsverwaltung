893f07b7cb1f9c87b24158e2fda9a1e6b9f5bdc1
AnsgarLichter <ansgar.lichter@dh-karlsruhe.de>
2019-03-28 13:21:52 +0100

    Added UserBean

--- Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/common/ejb/UserBean.java
+++ Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/common/ejb/UserBean.java
@@ -1,9 +1,9 @@
 /*
  * Copyright © 2018 Dennis Schulmeister-Zimolong
- * 
+ *
  * E-Mail: dhbw@windows3.de
  * Webseite: https://www.wpvs.de/
- * 
+ *
  * Dieser Quellcode ist lizenziert unter einer
  * Creative Commons Namensnennung 4.0 International Lizenz.
  */
@@ -21,13 +21,19 @@ import javax.persistence.PersistenceContext;
  * Spezielle EJB zum Anlegen eines Benutzers und Aktualisierung des Passworts.
  */
 @Stateless
-public class UserBean {
+public class UserBean extends EntityBean<User, String> {
 
     @PersistenceContext
     EntityManager em;
-    
+
     @Resource
     EJBContext ctx;
+
+    //<editor-fold defaultstate="collapsed" desc="Konstruktor">
+    public UserBean() {
+        super(User.class);
+    }
+    //</editor-fold>
 
     /**
      * Gibt das Datenbankobjekt des aktuell eingeloggten Benutzers zurück,
@@ -42,20 +48,23 @@ public class UserBean {
      *
      * @param username
      * @param password
+     * @param firstName
+     * @param lastName
      * @throws UserBean.UserAlreadyExistsException
      */
-    public void signup(String username, String password) throws UserAlreadyExistsException {
+    public void signup(String username, String password, String firstName, String lastName) throws UserAlreadyExistsException {
         if (em.find(User.class, username) != null) {
             throw new UserAlreadyExistsException("Der Benutzername $B ist bereits vergeben.".replace("$B", username));
         }
 
-        User user = new User(username, password);
+        User user = new User(username, password, firstName, lastName);
         user.addToGroup("app-user");
         em.persist(user);
     }
 
     /**
      * Passwort ändern (ohne zu speichern)
+     *
      * @param user
      * @param oldPassword
      * @param newPassword
@@ -68,25 +77,6 @@ public class UserBean {
         }
 
         user.setPassword(newPassword);
-    }
-    
-    /**
-     * Benutzer löschen
-     * @param user Zu löschender Benutzer
-     */
-    @RolesAllowed("app-user")
-    public void delete(User user) {
-        this.em.remove(user);
-    }
-    
-    /**
-     * Benutzer aktualisieren
-     * @param user Zu aktualisierender Benutzer
-     * @return Gespeicherter Benutzer
-     */
-    @RolesAllowed("app-user")
-    public User update(User user) {
-        return em.merge(user);
     }
 
     /**
