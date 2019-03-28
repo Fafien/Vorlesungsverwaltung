6ccdb95770234fc6cfd5b47f2497caad36419818
jonas <jonas@jonas-mbp>
2019-03-28 17:21:16 +0100

    AppointmentId durch id ersetzt

--- Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/lectures/jpa/Appointment.java
+++ Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/lectures/jpa/Appointment.java
@@ -6,8 +6,8 @@ import java.sql.Time;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -17,21 +17,21 @@ import javax.validation.constraints.NotNull;
  * @author jonas
  */
 @Entity
-@IdClass(AppointmentId.class)
 @Table(name = "VORLESUNGSVERWALTUNG_APPOINTMENT")
 public class Appointment implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
     @Id
+    @GeneratedValue
+    private long id;
+    
     @ManyToOne (fetch = FetchType.EAGER)
     private Lecture lecture;
     
-    @Id
     @NotNull(message = "Das Datum darf nicht leer sein.")
     private Date date;
 
-    @Id
     @NotNull(message = "Die Uhrzeit darf nicht leer sein.")
     private Time time;
     
