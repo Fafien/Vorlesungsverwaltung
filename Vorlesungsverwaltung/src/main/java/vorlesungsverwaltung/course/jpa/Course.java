d3f8a633462fbc177714d8cc531e322bc2bc6f24
AnsgarLichter <ansgar.lichter@dh-karlsruhe.de>
2019-03-28 13:53:04 +0100

    Course: imported lecture

--- Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/course/jpa/Course.java
+++ Vorlesungsverwaltung/src/main/java/vorlesungsverwaltung/course/jpa/Course.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import vorlesungsverwaltung.common.jpa.User;
+import vorlesungsverwaltung.lectures.jpa.Lecture;
 
 /**
  *
